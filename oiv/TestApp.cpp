e591af380d3bdc7694002b376bff7cc6a6fd9b47
Lior Lahav <LahavLior@gmail.com>
2016-09-18 23:18:40 +0300

    Added: option to browse files in the active folder

--- oiv/TestApp.cpp
+++ oiv/TestApp.cpp
@@ -1,29 +1,79 @@
 #include "PreCompiled.h"
 #include "TestApp.h"
+#include "Utility.h"
+#include <limits>
+#include <algorithm>
 namespace OIV
 {
+    
     template <class T,class U>
-    void TestApp::ExecuteCommand(CommandExecute command, T* request, U* response)
+    bool TestApp::ExecuteCommand(CommandExecute command, T* request, U* response)
     {
-        if (OIV_Execute(command, sizeof(T), request, sizeof(U), response)  != ResultCode::RC_Success)
-            throw std::exception("Api function failed");
+        return
+            OIV_Execute(command, sizeof(T), request, sizeof(U), response) == ResultCode::RC_Success;
+            
+    }
+
+    TestApp::TestApp()
+    {
+     
+    }
+
+    bool TestApp::LoadFile(std::wstring filePath)
+    {
+        CmdDataLoadFile loadFile;
+        loadFile.filePath = const_cast<OIVCHAR*>(filePath.c_str());
+        loadFile.FileNamelength = _tcslen(loadFile.filePath);
+        bool success  = ExecuteCommand(CommandExecute::CE_LoadFile, &loadFile, &CmdNull()) == true;
+
+        if (success)
+        {
+            using namespace Ogre;
+            RenderWindow* rw =  dynamic_cast<RenderWindow*>(Root::getSingleton().getRenderTarget("MainWindow"));
+
+            QryFileInformation fileInfo;
+            ExecuteCommand(CE_GetFileInformation, &CmdNull(), &fileInfo);
+            
+
+            std::wstringstream ss;
+            
+            ss << filePath <<L" | " << fileInfo.width << L" X " << fileInfo.height << L" X " << fileInfo.bitsPerPixel << L" BPP";
+            HWND handle = NULL;
+            rw->getCustomAttribute("WINDOW", &handle);
+            SetWindowTextW(handle, ss.str().c_str());
+        }
+
+        return success;
+    }
+
+    void TestApp::LoadFileInFolder(std::wstring filePath)
+    {
+        int idx = filePath.find_last_of(L"\\");
+        if (idx > -1)
+        {
+            std::wstring  folder = filePath.substr(0, idx);
+
+            Utility::find_files(folder, fListFiles);
+            fCurrentListPosition = std::find(fListFiles.begin(), fListFiles.end(), filePath);
+        }
+        else
+            fCurrentListPosition = fListFiles.end();
+
     }
 
     void TestApp::Run(std::wstring filePath)
     {
+        using namespace std;
         CmdDataInit init;
         init.parentHandle = NULL;
 
         ExecuteCommand(CommandExecute::CE_Init, &init, &CmdNull());
+        
+        LoadFile(filePath);
 
-        CmdDataLoadFile loadFile;
-        loadFile.filePath = const_cast<OIVCHAR*>(filePath.c_str());
-        loadFile.FileNamelength = _tcslen(loadFile.filePath);
+        
 
-        ExecuteCommand(CommandExecute::CE_LoadFile, &loadFile, &CmdNull());
-
-        QryFileInformation fileInfo;
-        ExecuteCommand(CE_GetFileInformation, &CmdNull(), &fileInfo);
+        LoadFileInFolder(filePath);
 
 
         HWND hwndMain;
@@ -55,6 +105,54 @@ namespace OIV
         std::cout << "Program ended.";
     }
 
+    void TestApp::JumpFiles(int step)
+    {
+        if (fListFiles.empty())
+            return;
+        
+        //std::list<tstring>::const_iterator
+        ListFilesIterator currentPos = fCurrentListPosition;
+
+        int sign;
+        if (step == std::numeric_limits<int>::max())
+        {
+            
+            currentPos = fListFiles.end(); // const_cast<ListFilesIterator&>(fListFiles.end());
+            sign = -1;
+        }
+        else if (step == std::numeric_limits<int>::min())
+        {
+            currentPos = fListFiles.begin();
+            sign = 1;
+        }
+        else
+        {
+            //currentPos = std::next(currentPos, step);
+            sign = step > 0 ? 1 : -1;
+        }
+
+        bool isLoaded = false;
+        do
+        {
+            if (currentPos == fListFiles.end() && sign == 1)
+                break;
+            if (currentPos == fListFiles.begin() && sign == -1)
+                break;
+            
+            currentPos = std::next(currentPos, sign);
+            
+            if (currentPos == fListFiles.end() && sign == 1)
+                break;
+            if (currentPos == fListFiles.begin() && sign == -1)
+                break;
+        }
+
+        while ((isLoaded = LoadFile(*currentPos)) == false);
+
+
+        fCurrentListPosition = currentPos;
+    }
+
     void TestApp::HandleMessages(const MSG & uMsg)
     {
         static int lastX = -1;// = std::numeric_limits<int>::min();
@@ -62,8 +160,30 @@ namespace OIV
         switch (uMsg.message)
         {
         case WM_KEYDOWN:
-            if (uMsg.wParam == VK_ESCAPE)
+            switch (uMsg.wParam)
+            {
+            case VK_ESCAPE:
                 PostQuitMessage(0);
+                break;
+            case VK_DOWN:
+            case VK_RIGHT:
+            case VK_NEXT:
+                JumpFiles(1);
+                break;
+            case VK_UP:
+            case VK_LEFT:
+            case VK_PRIOR:
+                JumpFiles(-1);
+                break;
+            case VK_HOME:
+                JumpFiles(std::numeric_limits<int>::min());
+                break;
+            case VK_END:
+                JumpFiles(std::numeric_limits<int>::max());
+                break;
+
+            }
+                
             break;
         case WM_MOUSEWHEEL:
         {
--- oiv/TestApp.h
+++ oiv/TestApp.h
@@ -5,10 +5,19 @@ namespace OIV
     class TestApp
     {
     public:
+        TestApp();
         void Run(std::wstring filePath);
+
+        void JumpFiles(int step);
         
         void HandleMessages(const MSG& uMsg);
         template<class T, class U>
-        void ExecuteCommand(CommandExecute command, T * request, U * response);
+        bool ExecuteCommand(CommandExecute command, T * request, U * response);
+
+    private:
+        ListFilesIterator fCurrentListPosition;
+        ListFiles fListFiles;
+        bool LoadFile(std::wstring filePath);
+        void LoadFileInFolder(std::wstring filePath);
     };
 }
