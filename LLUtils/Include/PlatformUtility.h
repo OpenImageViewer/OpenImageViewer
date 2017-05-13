95328826c2e2f27c3d1c8bd936b8e8d0102bbf38
Lior Lahav <LahavLior@gmail.com>
2017-05-13 12:09:24 +0300

    Added: windows - function to get last error string

--- LLUtils/Include/PlatformUtility.h
+++ LLUtils/Include/PlatformUtility.h
@@ -96,5 +96,27 @@ namespace LLUtils
 
             FindClose(hFile);
         }
+
+
+        
+            //Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+        static std::string GetLastErrorAsString()
+        {
+            //Get the error message, if any.
+            DWORD errorMessageID = ::GetLastError();
+            if (errorMessageID == 0)
+                return std::string(); //No error message has been recorded
+
+            LPSTR messageBuffer = nullptr;
+            size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+            std::string message(messageBuffer, size);
+
+            //Free the buffer.
+            LocalFree(messageBuffer);
+
+            return message;
+        }
     };
 }
