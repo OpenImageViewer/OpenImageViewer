OpenImageViewer/OpenImageViewer

commit ade7d427bc5f2208be1f070fc8127f675c5a55b6

Changed: show window at start-up only when image has been fully displayed. Changed: no auto refreshe - refresh of the renderer is now manually controlled.

OIViewer/TestApp.cpp
@@ -75,40 +75,54 @@ namespace OIV
     }
 
   
-    void TestApp::FinalizeImageLoad()
+    void TestApp::FinalizeImageLoad(ResultCode result)
     {
-        // Enter this function only from the main thread.
-        assert("TestApp::FinalizeImageLoad() can be called only from the main thread" &&
-            GetCurrentThreadId() == fMainThreadID);
-        
-        ImageHandle oldImage = fOpenedImage.imageHandle;
+        if (result == RC_Success)
+        {
+            // Enter this function only from the main thread.
+            assert("TestApp::FinalizeImageLoad() can be called only from the main thread" &&
+                GetCurrentThreadId() == fMainThreadID);
 
-        fOpenedImage = fImageBeingOpened;
-        fImageBeingOpened = ImageDescriptor();
+            ImageHandle oldImage = fOpenedImage.imageHandle;
+
+            fOpenedImage = fImageBeingOpened;
+            fImageBeingOpened = ImageDescriptor();
 
 
-        LLUtils::StopWatch stopWatch(true);
+            LLUtils::StopWatch stopWatch(true);
 
-         OIVCommands::DisplayImage(fOpenedImage.imageHandle
-             ,static_cast<OIV_CMD_DisplayImage_Flags>(OIV_CMD_DisplayImage_Flags::DF_ResetScrollState | OIV_CMD_DisplayImage_Flags::DF_ApplyExifTransformation));
-        
-         fOpenedImage.displayTime = stopWatch.GetElapsedTimeReal(LLUtils::StopWatch::TimeUnit::Milliseconds);
 
-        
-      
-        UpdateTitle();
-        UpdateStatusBar();
-        UpdateFileInddex();
+            OIVCommands::DisplayImage(fOpenedImage.imageHandle
+                , static_cast<OIV_CMD_DisplayImage_Flags>(
+                     OIV_CMD_DisplayImage_Flags::DF_ApplyExifTransformation
+                   | OIV_CMD_DisplayImage_Flags::DF_RefreshRenderer
+                   | OIV_CMD_DisplayImage_Flags::DF_ResetScrollState));
 
-        //Unload old image
-        OIVCommands::UnloadImage(oldImage);
+            fOpenedImage.displayTime = stopWatch.GetElapsedTimeReal(LLUtils::StopWatch::TimeUnit::Milliseconds);
+
+            UpdateTitle();
+            UpdateStatusBar();
+            UpdateFileInddex();
+
+            //Unload old image
+            OIVCommands::UnloadImage(oldImage);
+        }
+
+
+        if (fUpdateWindowOnInitialFileLoad == true)
+        {
+            //Show window on inital file load, whether success or failure.
+            fUpdateWindowOnInitialFileLoad = false;
+            fWindow.Show(true);
+        }
+
     }
 
-    void TestApp::NotifyImageLoaded()
+    void TestApp::FinalizeImageLoadThreadSafe(ResultCode result)
     {
         if (GetCurrentThreadId() == fMainThreadID)
         {
-            FinalizeImageLoad();
+            FinalizeImageLoad(result);
         }
         else
         {
@@ -116,7 +130,7 @@ namespace OIV
             std::unique_lock<std::mutex> ul(fMutexWindowCreation);
             
             // send message to main thread.
-            PostMessage(fWindow.GetHandle(), Win32::UserMessage::PRIVATE_WN_NOTIFY_LOADED, 0, 0);
+            PostMessage(fWindow.GetHandle(), Win32::UserMessage::PRIVATE_WN_NOTIFY_LOADED, result, 0);
         }
     }
 
@@ -155,8 +169,9 @@ namespace OIV
             fImageBeingOpened.loadTime = loadResponse.loadTime;
             fImageBeingOpened.imageHandle = loadResponse.handle;
             fImageBeingOpened.bpp = loadResponse.bpp;
-            NotifyImageLoaded();
+            
         }
+        FinalizeImageLoadThreadSafe(result);
 
         return result == RC_Success;
     }
@@ -185,7 +200,7 @@ namespace OIV
     }
 
 
-    void TestApp::Run(std::wstring relativeFilePath)
+    void TestApp::Init(std::wstring relativeFilePath)
     {
         using namespace std;
         using namespace placeholders;
@@ -206,7 +221,7 @@ namespace OIV
         }
         
         // initialize the windowing system of the window
-        fWindow.Create(GetModuleHandle(nullptr), SW_SHOW);
+        fWindow.Create(GetModuleHandle(nullptr), SW_HIDE);
         fWindow.AddEventListener(std::bind(&TestApp::HandleMessages, this, _1));
 
 
@@ -219,30 +234,46 @@ namespace OIV
         init.parentHandle = reinterpret_cast<std::size_t>(fWindow.GetHandleClient());
         ExecuteCommand(CommandExecute::CE_Init, &init, &CmdNull());
 
-        // Update client size
-        UpdateWindowSize(nullptr);
+        UpdateZoomScrollState();
 
-        // wait for initial file to finish loading
+        // Update client size
+        UpdateWindowSize();
+
+        // Show window when the initial file has loaded.        
+        if (isInitialFile == true)
+            fUpdateWindowOnInitialFileLoad = true; 
+
+        // Wait for initial file to finish loading
         if (asyncResult.valid())
             asyncResult.wait();
-        
+
+        //If there is no initial file, draw background and show window.
+        if (isInitialFile == false)
+        {
+            OIVCommands::Refresh();
+            fWindow.Show(true);
+        }
+
         // load settings
         fSettings.Load();
 
 
-        UpdateZoomScrollState();
-
         // Load all files in the directory of the loaded file
         if (filePath.empty() == false)
             LoadFileInFolder(filePath);
-        
-        Win32Helper::MessageLoop();
+    }
 
+
+    void TestApp::Destroy()
+    {
         // Destroy OIV when window is closed.
         ExecuteCommand(OIV_CMD_Destroy, &CmdNull(), &CmdNull());
     }
 
-
+    void TestApp::Run()
+    {
+        Win32Helper::MessageLoop();
+    }
 
     void TestApp::UpdateFileInddex()
     {
@@ -343,7 +374,10 @@ namespace OIV
         filter.filterType = static_cast<OIV_Filter_type>( std::min(OIV_Filter_type::FT_Count - 1,
             std::max(static_cast<int>(OIV_Filter_type::FT_None), static_cast<int>(filterType)) ));
         if (ExecuteCommand(CE_FilterLevel, &filter, &CmdNull()) == RC_Success)
+        {
             fFilterType = filter.filterType;
+            OIVCommands::Refresh();
+        }
     }
 
     void TestApp::ToggleGrid()
@@ -353,7 +387,7 @@ namespace OIV
         grid.gridSize = fIsGridEnabled ? 1.0 : 0.0;
         if (ExecuteCommand(CE_TexelGrid, &grid, &CmdNull()) == RC_Success)
         {
-            
+            OIVCommands::Refresh();
         }
         
     }
@@ -475,12 +509,14 @@ namespace OIV
         pan.x = horizontalPIxels;
         pan.y = verticalPixels;
         ExecuteCommand(CommandExecute::CE_Pan, &pan, &(CmdNull()));
+        OIVCommands::Refresh();
     }
 
     void TestApp::Zoom(double precentage, int zoomX , int zoomY )
     {
         CmdDataZoom zoom{ precentage,zoomX, zoomY };
         ExecuteCommand(CommandExecute::CE_Zoom, &zoom, &CmdNull());
+        OIVCommands::Refresh();
         UpdateCanvasSize();
     }
 
@@ -519,7 +555,7 @@ namespace OIV
     }
 
 
-    void TestApp::UpdateWindowSize(const Win32::EventWinMessage* winMessage)
+    void TestApp::UpdateWindowSize()
     {
             SIZE size = fWindow.GetClientSize();
             ExecuteCommand(CMD_SetClientSize,
@@ -556,8 +592,9 @@ namespace OIV
             fImageBeingOpened.source = ImageSource::IS_Clipboard;
             OIV_Util_GetBPPFromTexelFormat(texelFormat, &fImageBeingOpened.bpp);
             fImageBeingOpened.imageHandle = loadResponse.handle;
-            NotifyImageLoaded();
         }
+
+        FinalizeImageLoadThreadSafe(result);
 
         //return success;
 
@@ -708,9 +745,10 @@ namespace OIV
                 fImageBeingOpened.imageHandle = croppedHandle;
                 fImageBeingOpened.width = 7;
                 fImageBeingOpened.height= 7;
-                NotifyImageLoaded();
+                
             }
         }
+        FinalizeImageLoadThreadSafe(result);
 
     }
 
@@ -720,8 +758,8 @@ namespace OIV
         switch (uMsg.message)
         {
         case WM_SIZE:
-        //case WM_WINDOWPOSCHANGED:
-            UpdateWindowSize(evnt);
+            UpdateWindowSize();
+            OIVCommands::Refresh();
             break;
 
         case WM_TIMER:
@@ -729,7 +767,7 @@ namespace OIV
                 JumpFiles(1);
         break;
         case Win32::UserMessage::PRIVATE_WN_NOTIFY_LOADED:
-            NotifyImageLoaded();
+            FinalizeImageLoadThreadSafe(static_cast<ResultCode>( evnt->message.wParam));
         break;
         
         case Win32::UserMessage::PRIVATE_WN_AUTO_SCROLL:

OIViewer/OIVCommands.h
@@ -20,6 +20,11 @@ namespace OIV
             return OIV_Execute(command, sizeof(T), request, sizeof(U), response);
         }
 
+        static ResultCode OIVCommands::Refresh()
+        {
+                return ExecuteCommand(CommandExecute::CE_Refresh, &CmdNull(), &CmdNull());
+        }
+
         static ResultCode OIVCommands::UnloadImage(ImageHandle handle)
         {
             if (handle != ImageNullHandle)

OIViewer/TestApp.h
@@ -17,14 +17,15 @@ namespace OIV
     public:
         TestApp();
         ~TestApp();
+        void Init(std::wstring filePath);
+        void Run();
+        void Destroy();
         HWND GetWindowHandle() const;
         void UpdateTitle();
         void UpdateStatusBar();
         void UpdateZoomScrollState();
-        void Run(std::wstring filePath);
         void UpdateFileInddex();
         void JumpFiles(int step);
-
         void ToggleFullScreen();
         void ToggleBorders();
         void ToggleSlideShow();
@@ -35,7 +36,7 @@ namespace OIV
         void Zoom(double precentage, int zoomX = -1 , int zoomY = -1);
         void UpdateCanvasSize();
         void UpdateTexelPos();
-        void UpdateWindowSize(const Win32::EventWinMessage* winMessage);
+        void UpdateWindowSize();
 #pragma region Win32 event handling
         void handleKeyInput(const Win32::EventWinMessage* evnt);
 
@@ -50,8 +51,8 @@ namespace OIV
     private: //methods
         void OnScroll(LLUtils::PointI32 panAmount);
         bool LoadFile(std::wstring filePath, bool onlyRegisteredExtension);
-        void FinalizeImageLoad();
-        void NotifyImageLoaded();
+        void FinalizeImageLoad(ResultCode result);
+        void FinalizeImageLoadThreadSafe(ResultCode result);
         bool LoadFileFromBuffer(const uint8_t* buffer, const std::size_t size, std::string extension, bool onlyRegisteredExtension);
         void LoadFileInFolder(std::wstring filePath);
         void TransformImage(OIV_AxisAlignedRTransform transform);
@@ -80,5 +81,6 @@ namespace OIV
         LLUtils::ListString fListFiles;
         LLUtils::PointI32 fDragStart = { -1,-1 };
         UserSettings fSettings;
+        bool fUpdateWindowOnInitialFileLoad = false;
     };
 }

OIViewer/main.cpp
@@ -18,9 +18,11 @@ int mainFunction(int argc, const wchar_t** argv)
     }
 
     OIV::TestApp testApp;
-    testApp.Run(filePath);
+    testApp.Init(filePath);
+    testApp.Run();
+    testApp.Destroy();
+    
     return 0;
-
 }
 
 #ifdef WIN32

OIViewer/win32/Win32Window.cpp
@@ -299,6 +299,11 @@ namespace OIV
             return{ r.right - r.left, r.bottom - r.top };
         }
 
+        void Win32WIndow::Show(bool show) const
+        {
+            ShowWindow(fHandleWindow, show == true ?SW_SHOW : SW_HIDE);
+        }
+
         LRESULT Win32WIndow::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             Win32WIndow* window = reinterpret_cast<Win32WIndow*>(GetProp(hWnd, _T("windowClass")));

OIViewer/win32/Win32Window.h
@@ -52,7 +52,7 @@ namespace OIV
             void Move(const int16_t delta_x, const int16_t delta_y);
             LRESULT GetCorner(const POINTS& tag_points) const;
             LLUtils::PointI32 GetWindowSize() const;
-            
+            void Show(bool show) const;
             static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
             
             HWND DoCreateStatusBar(HWND hwndParent, int idStatus, HINSTANCE

oiv/API/defs.h
@@ -319,9 +319,10 @@ extern "C"
     ////
     enum OIV_CMD_DisplayImage_Flags
     {
-          DF_None = 0 << 0
-        , DF_ApplyExifTransformation = 1 << 0
-        , DF_ResetScrollState = 2 << 0
+          DF_None                       = 0 << 0
+        , DF_ApplyExifTransformation    = 1 << 0
+        , DF_ResetScrollState           = 1 << 1
+        , DF_RefreshRenderer            = 1 << 2
     };
 
     struct OIV_CMD_DisplayImage_Request

oiv/ApiGlobal.cpp
@@ -5,4 +5,3 @@ namespace OIV
     std::unique_ptr<IPictureRenderer> ApiGlobal::sPictureRenderer = std::unique_ptr<IPictureRenderer>(new OIV());
     CommandProcessor ApiGlobal::sCommandProcessor;
 }
-

commit 155725b3eab5ad158a960f4c13ee0c910ea44616

Fixed: issue introduced in ade7d42 - wrong client size, window must be manually refreshed since WM_SIZE is initially sent only when window becomes visible

OIViewer/TestApp.cpp
@@ -236,12 +236,18 @@ namespace OIV
 
         UpdateZoomScrollState();
 
+        
+        if (isInitialFile == true)
+        {
+            // delay window visibliy till the initial file is displayed.        
+            fUpdateWindowOnInitialFileLoad = true;
+
+            // Update the window size manually since the window won't receive WM_SIZE till it's visible.
+            fWindow.RefreshWindow();
+        }
+
         // Update client size
         UpdateWindowSize();
-
-        // Show window when the initial file has loaded.        
-        if (isInitialFile == true)
-            fUpdateWindowOnInitialFileLoad = true; 
 
         // Wait for initial file to finish loading
         if (asyncResult.valid())