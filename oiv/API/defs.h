7b21ad2246c54a7d5c2479120cda5ef2d171f00b
Lior Lahav <LahavLior@gmail.com>
2017-10-23 01:09:28 +0300

    Fixed: duplicate member field

--- oiv/API/defs.h
+++ oiv/API/defs.h
@@ -414,7 +414,6 @@ extern "C"
 
     struct OIV_CMD_QueryImageInfo_Response
     {
-        uint32_t RowPitchInBytes;
         uint32_t width;
         uint32_t height;
         uint32_t rowPitchInBytes;
