3705c896dd4c4a620dd3b1d559990775f4e123f1
Lior Lahav <LahavLior@gmail.com>
2018-10-04 09:48:49 +0300

    Added: Direct3D11 renderer support for RGBA texture format

--- OIVD3D11Renderer/Source/OIVD3DHelper.cpp
+++ OIVD3D11Renderer/Source/OIVD3DHelper.cpp
@@ -8,12 +8,26 @@ namespace OIV
         D3D11TextureSharedPtr OIVD3DHelper::CreateTexture(D3D11DeviceSharedPtr device, const IMCodec::ImageSharedPtr image, bool createMipMaps)
         {
 
-            if (image->GetImageType() != IMCodec::TexelFormat::I_R8_G8_B8_A8)
-                D3D11Error::HandleError("Direct3D11 renderer supports only RGBA pixel format");
+            DXGI_FORMAT textureFormat = DXGI_FORMAT_UNKNOWN;
+
+            switch (image->GetImageType())
+            {
+            case IMCodec::TexelFormat::I_B8_G8_R8_A8:
+                textureFormat = DXGI_FORMAT_B8G8R8A8_UNORM;
+                break;
+            case IMCodec::TexelFormat::I_R8_G8_B8_A8:
+                textureFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+                break;
+            default:
+                D3D11Error::HandleError("Unsupported texture format");
+            }
+
+            
+                
 
 
             D3D11Texture::CreateParams params;
-            params.format = DXGI_FORMAT_R8G8B8A8_UNORM;
+            params.format = textureFormat;
             params.width = image->GetWidth();
             params.height = image->GetHeight();
             params.mips = createMipMaps ? -1 : -2;
