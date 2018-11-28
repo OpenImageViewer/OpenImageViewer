54e87b0d6dbf09491cde624afd3e05803976815b
Lior Lahav <LahavLior@gmail.com>
2018-11-28 23:24:02 +0200

    Changed: limit Math class functions template instantiation

--- LLUtils/Include/MathUtil.h
+++ LLUtils/Include/MathUtil.h
@@ -8,35 +8,36 @@ namespace LLUtils
     class Math
     {
     public:
-        static constexpr double PI = 3.14159265358979323846;
-        template <typename T, typename std::enable_if_t<std::is_integral_v<T>, int> = 0>
-        constexpr LLUTILS_FORCE_INLINE static T Modulu(T val, T mod)
+        
+        static constexpr double PI =  3.14159265358979323846;
+        
+		template <typename T, typename std::enable_if_t<std::is_integral_v<T>, int> = 0>
+        static constexpr LLUTILS_FORCE_INLINE T Modulu(T val, T mod)
         {
             return (mod + (val % mod)) % mod;
         }
-        template <typename T, typename std::enable_if_t<std::is_floating_point_v<T>, int> = 0 >
-        constexpr LLUTILS_FORCE_INLINE static T Modulu(T val, T mod)
+        template <typename T, typename std::enable_if_t<std::is_floating_point_v<T>, int> = 0>
+        static constexpr LLUTILS_FORCE_INLINE T Modulu(T val, T mod)
         {
             return fmod(mod + fmod(val, mod), mod);
         }
 
-        template <typename T>
-        constexpr LLUTILS_FORCE_INLINE static T Sign(T val)
+        template <typename T, typename std::enable_if_t<std::is_arithmetic_v<T>, int> = 0>
+        static constexpr LLUTILS_FORCE_INLINE T Sign(T val)
         {
             return (static_cast<T>(0) < val) - (val < static_cast<T>(0));
         }
 
-        template <typename T>
-        constexpr LLUTILS_FORCE_INLINE static T ToDegrees(T val)
+        template <typename T, typename std::enable_if_t<std::is_arithmetic_v<T>, int> = 0>
+        static constexpr LLUTILS_FORCE_INLINE T ToDegrees(T val)
         {
-            return (val * 180) / PI;
+            return (val * static_cast<T>(180)) / static_cast<T>(PI);
         }
 
-        template <typename T>
-        constexpr LLUTILS_FORCE_INLINE static T ToRadians(T val)
+        template <typename T, typename std::enable_if_t<std::is_arithmetic_v<T>, int> = 0>
+        static constexpr LLUTILS_FORCE_INLINE T ToRadians(T val)
         {
-            return (val * PI) / 180;
+            return (val * static_cast<T>(PI)) / static_cast<T>(180);
         }
-
     };
 }
