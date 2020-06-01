e5bd66d3aaf26db33b4e63780f5fcf9c62c698c8
RandallFlagg <434681+RandallFlagg@users.noreply.github.com>
2020-06-01 03:14:03 +0300

    Change directory structure (#23)
    One click CMake to build them all
    Created an "External" directory for sub modules
    
    Co-authored-by: OG <O@G>

--- External/ImageCodec/Codecs/CodecDDS/Source/s3tc.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-void BlockDecompressImageDXT1(unsigned long width, unsigned long height, const unsigned char *blockStorage, unsigned long *image);
-void BlockDecompressImageDXT5(unsigned long width, unsigned long height, const unsigned char *blockStorage, unsigned long *image);
