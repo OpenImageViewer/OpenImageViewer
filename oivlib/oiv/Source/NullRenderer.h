d636d929fe09e96c81ee8685cf817369a36f7bf9
RandallFlagg <O@G>
2023-12-05 01:09:11 +0200

    OIVLib now compiles on Linux.

--- oivlib/oiv/Source/NullRenderer.h
+++ oivlib/oiv/Source/NullRenderer.h
@@ -13,6 +13,7 @@ namespace OIV
         int SetFilterLevel([[maybe_unused]] OIV_Filter_type filterLevel) override { return 0; }
         int SetExposure([[maybe_unused]] const OIV_CMD_ColorExposure_Request & exposure) override { return 0; }
         int SetSelectionRect([[maybe_unused]] VisualSelectionRect selectionRect) override { return 0; }
+        int SetBackgroundColor(int index, LLUtils::Color backgroundColor) override {return 0;}
 
         int AddRenderable([[maybe_unused]] IRenderable* renderable) override { return 0; }
         int RemoveRenderable([[maybe_unused]] IRenderable* renderable) override { return 0; }
